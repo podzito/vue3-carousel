2573712f0b28e73ce2f5ccd09bc6ff800f2957e4
Arshad Mahmood <1391251+arshadm@users.noreply.github.com>
2024-06-20 09:36:51 +0100

    updated project to use vue/vue-jest@29

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,27 @@
-const { pathsToModuleNameMapper } = require('ts-jest')
+import { readFileSync } from 'fs'
+import { dirname, join } from 'path'
+import { pathsToModuleNameMapper } from 'ts-jest'
+import { fileURLToPath } from 'url'
 
-const { compilerOptions } = require('./tsconfig.json')
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
-module.exports = {
+const tsconfig = JSON.parse(readFileSync(join(__dirname, 'tsconfig.json'), 'utf-8'))
+
+export default {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  testEnvironmentOptions: {
+    customExportConditions: ['node', 'node-addons'],
+  },
   testMatch: ['<rootDir>/**/*.spec.[jt]s?(x)'],
+  transform: {
+    '^.+\\.vue$': '@vue/vue3-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+  },
   collectCoverageFrom: ['src/**/*.ts'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths, {
     prefix: '<rootDir>',
   }),
 }
--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,15 @@
+import { readFileSync } from 'fs'
+import { dirname, join } from 'path'
 import css from 'rollup-plugin-css-only'
 import del from 'rollup-plugin-delete'
 import dts from 'rollup-plugin-dts'
 import { typescriptPaths } from 'rollup-plugin-typescript-paths'
 import typescript from 'rollup-plugin-typescript2'
+import { fileURLToPath } from 'url'
 
-import pkg from './package.json' assert { type: 'json' }
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+const pkg = JSON.parse(readFileSync(join(__dirname, 'package.json'), 'utf-8'))
 
 const banner = `/**
  * Vue 3 Carousel ${pkg.version}
